c5bb6f4ce29717c0054c1d7621648adfc38e495b
Enxhi <enxhi@themobilelife.com>
2024-08-20 10:18:54 +0200

    Initial Commit

---
+++ Sole/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sole
+//
+//  Created by Enxhi Qemalli on 20.8.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
